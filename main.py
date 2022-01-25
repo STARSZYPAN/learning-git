e8a53c06737b30137b5b1c8d5439b122d03a940f
STARSZYPAN <98351502+STARSZYPAN@users.noreply.github.com>
2022-01-25 22:31:57 +0100

    witam

--- main.py
+++ main.py
@@ -1,11 +1,1 @@
-lista_a={
-'piekarnia':['bulki','chlebek','paczek'],
-'warzywniak':['buraki','fasola','ziemniaki']
-}
-
-lista_dict=dict(lista_a)
-print('Lista zakupow') 
-for sklep,rzeczy in lista_dict.items(): 
-    print('Kiedy ide do' ,(sklep.upper()),',kupuje tam takie produkty jak:',(rzeczy))
-print('Idac do sklepu ,kupilem razem',len(rzeczy)*2,'rzeczy')
-
+print("witam mentora")
