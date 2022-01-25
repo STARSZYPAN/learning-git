44084e2a2345fa57a2eef6926e080f6cf4c2aa4a
STARSZYPAN <98351502+STARSZYPAN@users.noreply.github.com>
2022-01-25 21:54:55 +0100

    Update main.py

--- main.py
+++ main.py
@@ -8,3 +8,4 @@ print('Lista zakupow')
 for sklep,rzeczy in lista_dict.items(): 
     print('Kiedy ide do' ,(sklep.upper()),',kupuje tam takie produkty jak:',(rzeczy))
 print('Idac do sklepu ,kupilem razem',len(rzeczy)*2,'rzeczy')
+print('hiszpanska inkwizycja rulez')
